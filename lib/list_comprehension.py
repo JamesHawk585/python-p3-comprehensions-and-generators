98558e2670a40a4d5d9b68cfc6a17c2c8daaecef
James Hawk <jph94880@gmail.com>
2023-07-20 20:43:27 -0500

    Passes each of the two tests for return_evens().

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
+# return_evens([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+# squared_lc = [n ** 2 for n in range]
 
 def return_evens(num_list):
-    pass
+    even_num_list = [n for n in num_list if n % 2 ==0]
+    return even_num_list
 
 def make_exclamation(sentence_list):
     pass
